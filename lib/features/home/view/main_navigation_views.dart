ec97060845d72cf4a12d83999f4794dd87ab215b
Bosina Mahmoud <146219409+Bosinamahmoud@users.noreply.github.com>
2026-03-10 23:39:03 +0200

    add bottom nav bar

--- lib/features/home/view/main_navigation_views.dart
+++ lib/features/home/view/main_navigation_views.dart
@@ -1,4 +1,6 @@
 import 'package:cureta/core/Services/GetItServices.dart';
+import 'package:cureta/core/localization/app_localizations.dart';
+import 'package:cureta/features/medical_records/widgets/user_records_bottom_navigation.dart';
 import 'package:cureta/features/profile/data/repo/profile_repository.dart';
 import 'package:cureta/features/profile/view/profile_detail_view.dart';
 import 'package:cureta/features/profile/view_model/profile_list_cubit.dart';
@@ -101,6 +103,29 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
             ),
           ),
           body: IndexedStack(index: _selectedIndex, children: _screens),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          bottomNavigationBar: UserRecordsBottomNavigation(
+            currentIndex: _selectedIndex,
+            homeLabel: AppLocalizations.recordsListNavHome,
+            medsLabel: AppLocalizations.recordsListNavMeds,
+            scanRxLabel: AppLocalizations.recordsListNavScanRx,
+            recordsLabel: AppLocalizations.recordsListNavRecords,
+            profileLabel: AppLocalizations.recordsListNavProfile,
+            onHomePressed: () {
+              _onItemTapped(0);
+            },
+            onMedsPressed: () {
+              _onItemTapped(1);
+            },
+
+            onRecordsPressed: () {
+              _onItemTapped(2);
+            },
+            onProfilePressed: () {
+              _onItemTapped(3);
+            },
+          ),
         ),
       ),
     );
